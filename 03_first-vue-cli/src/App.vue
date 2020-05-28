5bb839c03ea558a5173552c3cbcfdbb40bc4c9ac
chloe-codes1 <juhyun.kim@lindsey.edu>
2020-05-29 00:45:24 +0900

    Add vue-cli exercises

--- 03_first-vue-cli/src/App.vue
+++ 03_first-vue-cli/src/App.vue
@@ -1,27 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <!-- step 3: 사용한다 -->
-    <MyComponent />
-    <GreatComponent/>
+    <div id="nav">
+      <router-link to="/">Home </router-link>
+      <router-link to="/about">About </router-link>
+      <router-link to="/contact">Contact </router-link>
+      <router-link to="/ping">Ping </router-link>
+    </div>
+    <hr>
+    <!-- 여기에 =/= block content -->
+    <router-view />
+    <!-- component rendering -->
+    <hr>
   </div>
 </template>
 
 <script>
 // [Component 사용하기]
 // step 1: import 한다
-import HelloWorld from './components/HelloWorld.vue'
-import MyComponent from './components/MyComponent.vue'
-import GreatComponent from './components/GreatComponent.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import MyComponent from './components/MyComponent.vue'
+// import GreatComponent from './components/GreatComponent.vue'
 
 export default {
   name: 'App',
   // step 2: 등록한다
   components: {
-    HelloWorld,
-    MyComponent,
-    GreatComponent,
+    // HelloWorld,
+    // MyComponent,
+    // GreatComponent,
   }
 }
 </script>
@@ -33,6 +39,20 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+
 </style>
